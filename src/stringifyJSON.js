a7c5700a99bbb722c2c94c1c0e31d1e35d558cd2
bruceisyoung <bruce.isyoung2016@gmail.com>
2016-08-25 15:47:05 -0700

    implemented stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,25 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if(obj===undefined)
+  	return;
+  if(typeof(obj)==="function")
+  	return;
+  if(typeof(obj)==="number" || typeof(obj)==="boolean")
+  	return obj.toString();
+  if(obj===null)
+  	return 'null';
+  if(typeof(obj)==="string")
+  	return "\"" + obj + "\"";
+  if(Array.isArray(obj))
+  	return "["+obj.map(function(item){ return stringifyJSON(item);}).join(",")+"]";
+  if(typeof(obj)==="object"){
+  	var result = [];
+  	for (var key in obj){
+  		if(typeof(obj[key])!="function" && obj[key]!==undefined)
+  			result.push((stringifyJSON(key) + ":" + stringifyJSON(obj[key])));
+  	}
+  	return "{"+result.join(",")+"}";
+  }
+
 };
