82d8810f7c39d4c4670788b334f99ec4407d5228
bruceisyoung <bruce.isyoung2016@gmail.com>
2016-08-25 19:18:23 -0700

    Finished the function of getElementsByClassName. Besides the functions mentioned, also used nodetype to get rid of the text node.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,21 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className) {
   // your code here
+  var elementList = [];
+  var result = [];
+  function listElement(item) {
+    elementList.push(item);
+  	if(item.childNodes.length>0){
+      for(var i=0; i<item.childNodes.length; i++)
+  	    listElement(item.childNodes[i]);
+  	}	
+  }
+  listElement(document.body);
+  for(var i=0; i<elementList.length; i++){
+  	if(elementList[i].nodeType!==3&&elementList[i].classList.contains(className))
+  	  result.push(elementList[i]);
+  }
+  return result;
 };
